14048c0d3a1fbee3464f939539d2d2896c51ec73
zelstart <e.pomeroy95@gmail.com>
2023-09-25 15:44:44 -0400

    findAll for categories as well as findByPk

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,17 +1,46 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+router.get('/', (req, res) => {
+  Category.findAll({
+    attributes: ['id', 'category_name'],
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }]
+  })
+  .then(categoryData => {
+    res.json(categoryData);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
 });
 
-router.get('/:id', (req, res) => {
+
   // find one category by its `id` value
   // be sure to include its associated Products
+router.get('/:id', (req, res) => {
+  Category.findByPk(req.params.id, {
+    include: [{
+      model: Product
+    }]
+  })
+  .then(categoryById => {
+    if (!categoryById) {
+      res.status(404).json({ message: 'No category found with that id!'})
+    } else {
+      res.status(200).json(categoryById);
+    }
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
 });
+
+
 
 router.post('/', (req, res) => {
   // create a new category
