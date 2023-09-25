d8ae28cd99a3d39358ccab14234e2e430f558189
zelstart <e.pomeroy95@gmail.com>
2023-09-25 16:16:41 -0400

    update + delete categories routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,14 +44,52 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then((newCategory) =>
+    res.json(newCategory))
+  .catch((err) => {
+    res.json(err)
+  })
 });
 
+
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+    // update a category by its `id` value
+  Category.update(
+    { category_name: req.body.category_name },
+    { where: { id: req.params.id } } 
+  )
+  .then(updatedCategory => {
+    if (updatedCategory[0] === 0) {
+      res.status(404).json({ message: 'No category found with that id!' });
+    } else {
+      res.status(200).json({ message: 'Category updated successfully!' });
+    }
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(deletedCategory => {
+    if (deletedCategory === 0) {
+      res.status(404).json({ message: 'No category found with that id!' });
+    } else {
+      res.status(200).json({ message: 'Category deleted successfully!' });
+    }
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
