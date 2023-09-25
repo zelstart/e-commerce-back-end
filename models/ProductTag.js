e34b238224ecd8ee1e7e78b261d0dd2c4af065a2
zelstart <e.pomeroy95@gmail.com>
2023-09-25 14:47:12 -0400

    set up columns for productTag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,16 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
